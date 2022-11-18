7712d13b8440633a573444463d34d515f58fe383
DmytroVoitovych <begunec23@gmail.com>
2022-11-19 00:45:46 +0200

    fix length password

--- validation/dataValidation.js
+++ validation/dataValidation.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 const validation = (data) => {
     const shema = Joi.object({
         email: Joi.string().min(4).max(255).required().email(),
-        password: Joi.string().min(6).max(255).required(),
+        password: Joi.string().min(6).max(12).required(),
      });
 
     return shema.validate(data);
