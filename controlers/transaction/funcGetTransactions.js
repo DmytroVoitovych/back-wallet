4492460835ca5145c0e5dfd04318d5c74324ff2e
DmytroVoitovych <begunec23@gmail.com>
2022-11-21 19:03:19 +0200

    ready pagination

--- controlers/transaction/funcGetTransactions.js
+++ controlers/transaction/funcGetTransactions.js
@@ -1,21 +1,24 @@
 const {Action} = require('../../models/transaction');
 
 const funcGetTransaction = async ({ user, query }, res) => {
-  
+
     const { page = 1, limit = 10,  } = query; // пагинация
     const skip = (page - 1) * limit;
-    // const fav = query.favorite;
-   
-    // if (fav !== undefined) { //  фильтрация будет работать если она включена в строку запроса иначе код ниже дефолтный
-    //     return res.json({
-    //     status: 200,
-    //   data: await Contact.find({ owner: user._id, favorite: fav},'',{skip, limit: +limit, }).populate('owner', '_id email'),
-    // })
-    // }
+     
+    if (query.page === undefined) { //отдаю все транзакции при ендпоинте без пагинации
+        return res.json({
+      status: 200,
+      alllist:  await Action.find({ owner: user._id })
+    })
+    }
+  
+  const data = await (await Action.find({ owner: user._id }, 'date category type comment sum balance _id', { skip, limit: +limit }).sort({ _id: -1 })).reverse();
+  const length = data.length;
 
   return res.json({ // отобразить колекцию если параметр favorite не установлен 
-        status: 200,
-      data: await (await Action.find({ owner: user._id},'date category type comment sum balance',{skip, limit: +limit}).sort( { _id: -1 } )),
+    status: 200,
+    data,
+    page: length === 0?`sorry this page ${page} empty`:page 
     })
 }; // find без аргументов возращает всю колекцию
 // const funcGetContacts = async (_, res) =>  res.json({ status: 200, data: await Contact.find({},'name email phone'), }); // вторым аргументом можна передать определенные поля для вывода с колекции
