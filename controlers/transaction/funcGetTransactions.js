0be8a0a30ccc8a4dcd4963e237d8554391085dde
DmytroVoitovych <begunec23@gmail.com>
2022-11-21 11:53:34 +0200

    add sort

--- controlers/transaction/funcGetTransactions.js
+++ controlers/transaction/funcGetTransactions.js
@@ -2,8 +2,8 @@ const {Action} = require('../../models/transaction');
 
 const funcGetTransaction = async ({ user, query }, res) => {
   
-    // const { page = 1, limit = 20,  } = query; // пагинация
-    // const skip = (page - 1) * limit;
+    const { page = 1, limit = 10,  } = query; // пагинация
+    const skip = (page - 1) * limit;
     // const fav = query.favorite;
    
     // if (fav !== undefined) { //  фильтрация будет работать если она включена в строку запроса иначе код ниже дефолтный
@@ -15,7 +15,7 @@ const funcGetTransaction = async ({ user, query }, res) => {
 
   return res.json({ // отобразить колекцию если параметр favorite не установлен 
         status: 200,
-      data: await Action.find({ owner: user._id},''),
+      data: await (await Action.find({ owner: user._id},'date category type comment sum balance',{skip, limit: +limit}).sort( { _id: -1 } )),
     })
 }; // find без аргументов возращает всю колекцию
 // const funcGetContacts = async (_, res) =>  res.json({ status: 200, data: await Contact.find({},'name email phone'), }); // вторым аргументом можна передать определенные поля для вывода с колекции
