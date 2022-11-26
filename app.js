6f1c846db44a985169d9bcafce4ebd8c2273f488
DmytroVoitovych <begunec23@gmail.com>
2022-11-26 19:03:19 +0200

    ready swager

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger/openapi.json');
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -15,7 +17,7 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 app.use(logger(formatsLogger));
 app.use(cors()); 
 app.use(express.json());
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/api/user/auth', contactsAuthRouter); //роутер для работы с логинизацией
 app.use('/api/transactions', transactionsRouter); //роутер для работы с транзакцией
@@ -28,5 +30,7 @@ app.use((err, req, res, next) => {
   const { status=500, message='Server error' } = err;
   res.status(status).json({status: 'failed', code: status, message, })
 });
+
+
 
 module.exports = app;
